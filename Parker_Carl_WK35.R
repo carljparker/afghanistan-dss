7d6be80b10b57a46d8852566b82c57b99c10a09e
Carl Parker <carljparker@fb.com>
2021-09-03 21:56:34 -0700

    Remove extraneous code and add first draft of Tumblr viz

--- Parker_Carl_WK35.R
+++ Parker_Carl_WK35.R
@@ -1,97 +1,7 @@
-# 
-# Carl Parker
-# carljparker@gmail.com
-# 206.388.6677
-#
-# Data 567: Applied Statistics and Experimental Design
-# 
-# Homework <homework number>
-# 
-
-#
-# Template for this file is located at:
-#
-#   ~/git/gtdev/R-language/templates
-#
-
-#
-# Add an initial @knitr section
-#
-
-## @knitr Prolog 
-#
-# Need to set directory for this @knitr section to do the `source()`
-# below.
-#
-setwd( "/Users/carljparker/git/dss/data-sci/afghanistan-dss" )
-getwd()
 
 
-#
-# --- Sourced Files ---
-#
-source( "code/preamble.R" )
-
-
-#
-# --- Library Dependencies ---
-#
-
-
-#
-# --- Function Definitions ---
-#
-
-
-#
-# If I run this in RStudio or in vimR, interactive()
-# will return TRUE.
-#
-# If I run this with my "rr" shell function, interactive() 
-# will return FALSE.
-#
-if ( interactive() ) {
-
- #
- # Set the working directory. Only need to do this for RStudio or RMarkdown.
- #
- # For RMarkdown, need to do this for each chunk.
- #
- setwd( "/Users/carljparker/git/dss/data-sci/afghanistan-dss" )
- getwd()
-
-}  # if interactive
-
-
-#
-# Set repeatable random seed using my zip code.
-#
-set.seed(98122)
-
-
-#
-# Initialize logging functionality
-#
-logger.init()
-
-
-## @knitr q0a 
-#
-# Set working directory for this knitr chunk
-#
-setwd( "/Users/carljparker/git/dss/data-sci/afghanistan-dss" )
-
-
-#
-# --- Get the data ---
-#
-logdebug( "Read data from CSV." )
-
-
-#
-# Update the following line with the name of the CSV file to read.
-#
 csv.data.file.ch <- "data/deaths-afghanistan.txt"
+
 csv.data.df <- read.csv( 
                          csv.data.file.ch, 
                          header = TRUE, 
@@ -154,28 +64,6 @@ color <- c(
 
 csv.data.df <- cbind( csv.data.df, pres, color )
 
-#
-# --- Explore the data ---
-#
-
-#
-# The following line is a nice way to set off 
-# data exploration code inside a production
-# context.
-#
-
-# -*- -*- -*- #
-
-dex.dataframe( csv.data.df )
-
-# -*- -*- -*- #
-
-
-## @knitr q1a
-#
-# Set working directory for this knitr chunk
-#
-setwd( "/Users/carljparker/git/dss/data-sci/afghanistan-dss" )
 
 #
 # Afghanistan deaths: Viz for GitHub (large)
@@ -231,39 +119,56 @@ viz.afghan.deaths.large( "viz/afghanistan.deaths-large.png" )
 dev.off()
 
 
-## @knitr q1b 
-
-
-## @knitr q1c 
-
-
-## @knitr q1d 
-
-
-
-## @knitr q2a 
 #
-# Set working directory for this knitr chunk
+# Afghanistan deaths: Viz for GitHub (Tumblr)
 #
-setwd( "/Users/carljparker/git/dss/data-sci/afghanistan-dss" )
+viz.afghan.deaths.tumblr <- function( out.name ) {
 
+  png( out.name, 
+       width = 500, height = 375, 
+       units = "px", pointsize = 9, 
+       bg = rgb( 248, 245, 236, maxColorValue = 255 ) 
+  )
 
-## @knitr q2b 
+  par( 
+      cex.main = 2.5, cex.axis = 1.75, cex.lab = 2,
+      # margins: bottom, left, top and right
+      par( mar = c( 6, 7, 4, 4 ) + 0.1 )  
+  )
 
+  plot( 
+       csv.data.df$Year, csv.data.df$US,
+       ylim = c( 0, 500 ),
+       main = "US military deaths in Afghanistan by year",
+       xlab = "", ylab = "",
+       pch = 24, cex=3, col="black", bg=csv.data.df$color, lwd=2,
+       # Remove the x-axis so we can customize it further below
+       xaxt = "n"  
+  )
 
-## @knitr q2c 
+  title( xlab="Year", cex.lab = 3, line = 5 )
+  title( ylab="Number of deaths", cex.lab = 3, line = 5 )
 
+  #
+  # Customize the x-axis
+  #
+  axis( 1, at = seq(2001, 2021, 4 ), labels = seq(2001, 2021, 4 ) )
 
-## @knitr q2d 
+  legend(
+          "topright", 
+          title = "Presidents",
+          bg = "white",
+          legend = c( "Bush", "Obama", "Trump", "Biden" ),
+          pch = 24,
+          pt.cex = 3,
+          pt.bg = c( "Red", "Blue", "Orange", "Green" )
+        )
 
+}
 
-## @knitr Epilog
-#
-# Set working directory for this knitr chunk
-#
-setwd( "/Users/carljparker/git/dss/data-sci/afghanistan-dss" )
-
-logdebug( "Program complete." )
+viz.afghan.deaths.tumblr( "viz/afghanistan.deaths-tumblr.png" )
+                         
+dev.off()
 
 
 # --- END --- #
